95867c26b709c8f96adc3ff5c3a996cc4ef960f7
Nahom Alem <alemnahom@outlook.com>
2022-06-15 14:43:39 +0100

    added form

---
+++ components/Form/Form.tsx
@@ -0,0 +1,55 @@
+export default function Form() {
+    return (
+      <form action="/api/form" method="post">
+        <input type="text" name="taskInput" placeholder="Please write a task"/>
+        <button type="submit">Submit</button>
+      </form>
+    )
+  }
+
+//   export default function Form() {
+//     // Handles the submit event on form submit.
+//     const handleSubmit = async (event:any) => {
+//       // Stop the form from submitting and refreshing the page.
+//       event.preventDefault()
+  
+//       // Get data from the form.
+//       const data = {
+//         taskInput: event.target.taskInput.value,
+//       }
+  
+//       // Send the data to the server in JSON format.
+//       const JSONdata = JSON.stringify(data)
+  
+//       // API endpoint where we send form data.
+//       const endpoint = '/api/form'
+  
+//       // Form the request for sending data to the server.
+//       const options = {
+//         // The method is POST because we are sending data.
+//         method: 'POST',
+//         // Tell the server we're sending JSON.
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         // Body of the request is the JSON data we created above.
+//         body: JSONdata,
+//       }
+  
+//       // Send the form data to our forms API on Vercel and get a response.
+//       const response = await fetch(endpoint, options)
+  
+//       // Get the response data from server as JSON.
+//       // If server returns the name submitted, that means the form works.
+//       const result = await response.json()
+//       alert(`Task to do: ${result.data}`)
+//     }
+//     return (
+//       // We pass the event to the handleSubmit() function on submit.
+//       <form onSubmit={handleSubmit}>
+//         <input type="text" name="taskInput" placeholder="Please write a task"/>
+//         <button type="submit">Submit</button>
+//       </form>
+//     )
+//   }
+  
